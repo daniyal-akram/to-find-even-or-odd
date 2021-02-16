54c4231a0c401bcbc0fe6df08fce93ad2f0007f4
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-02-16 16:38:07 +0500

    Add files via upload

---
+++ check it is even or odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int num;
+  cout<<"\nEnter a Number:";
+  cin>>num;
+  if(num%2==0)
+  {
+  	 cout<<"You Enter an Even Number:"<<num;
+  }else{
+  	cout<<"You Enter an ODD Number:"<<num;
+}
+}
